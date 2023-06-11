b974f4186c15a3808d7734fcbabede76154b8c60
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-06-11 21:26:22 +0600

    login page done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,22 @@
 import Lottie from "lottie-react";
-import registerAnnimation from '../../../public/register.json'
+import registerAnnimation from "../../../public/register.json";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  
+  const onSubmit = (data) => {
+    console.log(data);
+
+  };
+
   return (
     <div>
       <div>
@@ -12,7 +26,10 @@ const Register = () => {
               <Lottie animationData={registerAnnimation} loop={true} />
             </div>
             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-              <form  className="card-body glass">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="card-body glass"
+              >
                 <div className="form-control">
                   <label className="label">
                     <span className="font-garamond">Name</span>
@@ -20,9 +37,13 @@ const Register = () => {
                   <input
                     type="text"
                     placeholder="Type Your Name"
+                    {...register("name", { required: true })}
                     name="name"
                     className="input input-bordered required"
                   />
+                  {errors.name && (
+                    <span className="text-red-500">Name is required</span>
+                  )}
                 </div>
                 <div className="form-control">
                   <label className="label">
@@ -31,9 +52,13 @@ const Register = () => {
                   <input
                     type="email"
                     placeholder="Type Your Email"
+                    {...register("email", { required: true })}
                     name="email"
                     className="input input-bordered required"
                   />
+                  {errors.email && (
+                    <span className="text-red-500">Email is required</span>
+                  )}
                 </div>
                 <div className="form-control">
                   <label className="label">
@@ -43,8 +68,12 @@ const Register = () => {
                     type="url"
                     placeholder="Insert Your Photo URL"
                     name="photo"
+                    {...register("photo", { required: true })}
                     className="input input-bordered required"
                   />
+                  {errors.photo && (
+                    <span className="text-red-500">Photo is required</span>
+                  )}
                 </div>
                 <div className="form-control">
                   <label className="label">
@@ -54,19 +83,34 @@ const Register = () => {
                     type="password"
                     placeholder="Type Your Password"
                     name="password"
+                    {...register(
+                      "password",
+                      { required: true , pattern:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                      }
+                    )}
                     className="input input-bordered required"
                   />
+                  {errors.password?.type === "pattern" && (
+                    <span className="text-red-500">
+                      Please Enter Minimum eight characters, at least one letter, one number and one special character.
+                    </span>
+                  )}
                 </div>
-                              <div className="form-control">
-                              <label className="label">
+                <div className="form-control">
+                  <label className="label">
                     <span className="font-garamond">Confirm Your Password</span>
                   </label>
                   <input
+                    {...register("confirmPassword", { required: true, validate: (value) =>
+                      value === watch("password") || "Passwords do not match" })}
                     type="password"
                     placeholder="Confirm Your Password"
-                    name="password"
+                    name="confirmPassword"
                     className="input input-bordered required"
                   />
+                    {errors.confirmPassword && (
+          <span className="error text-red-500">{errors.confirmPassword.message}</span>
+        )}
                 </div>
                 <div className="form-control mt-6">
                   <input
@@ -74,8 +118,13 @@ const Register = () => {
                     value="Register"
                     className="btn btn-outline border-0 border-l-4 border-sky-400 text-sky-400 hover:bg-sky-400 hover:border-cyan-400 hover:text-white font-cinzel"
                   />
-                              </div>
-                              <p>Already Registred ??? <Link to={'/login'}><span>Login Here!!!</span></Link></p>
+                </div>
+                <p>
+                  Already Registred ???{" "}
+                  <Link to={"/login"}>
+                    <span>Login Here!!!</span>
+                  </Link>
+                </p>
               </form>
             </div>
           </div>
